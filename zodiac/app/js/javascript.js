6a34a051bf875d2a82fe6b6ec293b57fa3f78017
Hunter <hmh84@drexel.edu>
2020-01-27 23:26:36 -0500

    JS, Sounds, Fixes :wrench:
    -Optimized JS for welcome screen
    -Added "help" button to reopen welcome screen
    -Fixed dimmed welcome screen background
    -Trimmed all mp3 files to desired selections and lengths

--- zodiac/app/js/javascript.js
+++ zodiac/app/js/javascript.js
@@ -1,4 +1,14 @@
-function toggleWelcome() {
-    var element = document.getElementById("welcome");
-    element.classList.toggle("welcome_inactive");
+function toggleWelcomeOff() {
+    document.getElementById('welcome').hidden = true;
+}
+function toggleWelcomeOn() {
+    document.getElementById('welcome').removeAttribute('hidden');
+}
+function indicateSelection() {
+
+}
+
+function indicateQuerie() {
+    document.getElementById('background_1').id('id', 'background_0');
+    console.log('queried')
 }
